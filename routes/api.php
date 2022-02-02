cde3ce75e46eef8eff27554df45cc15a6853e245
jopanthe <jonathanpandu6@gmail.com>
2022-02-02 13:59:52 +0700

    uptdate

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+use App\Http\Controllers\PostsApiController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/posts', [PostsApiController::class, 'index']);
+
+Route::post('/posts', [PostsApiController::class, 'store']);
+
+Route::put('/posts/{post}', [PostsApiController::class, 'update']);
+
+Route::delete('/posts/{post}', [PostsApiController::class, 'destroy']);
